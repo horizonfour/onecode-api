590a5c2d9a1fd8d79fe9d9fb991ea393c480f844
Igor Franca <lee12rock@gmail.com>
2017-10-16 17:39:20 -0200

    Adicionado alguns comentários

--- src/routes/HelloWolrdRoute.ts
+++ src/routes/HelloWolrdRoute.ts
@@ -3,9 +3,14 @@ import IRoute from './interfaces/IRoute';
 
 class HelloWorldRoute implements IRoute {
   private helloController: HelloWorldController;
+
   constructor() {
     this.helloController = new HelloWorldController();
   }
+  /**
+   * @returns [Returns the Route object for HapiRouter to setup]
+   * @memberOf HelloWorldRoute
+   */
   private getHelloWord() {
     return [
       {
